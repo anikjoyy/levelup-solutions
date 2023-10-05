e92f5d91e7129a69b4e36fcad2c714105ad2d915
anikjoyy <anikjoy.cse@gmail.com>
2023-10-05 22:14:23 +0600

    created price section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Features from './Features';
 import Hero from './Hero';
+import Prices from './Prices';
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Features />
+      <Prices />
     </div>
   );
 };
