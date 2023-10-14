91c80fc442ec758045ee0519d6a788d698fd5d79
anikjoyy <anikjoy.cse@gmail.com>
2023-10-14 15:50:08 +0600

    asfe sdf

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../layout/Main';
 import Home from '../pages/Home/Home';
+import About from '../pages/About/About';
+import Services from '../pages/Services/Services';
+import Contact from '../pages/Contact/Contact';
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +13,18 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'services',
+        element: <Services />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
       },
     ],
   },
